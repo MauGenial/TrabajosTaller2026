f7fe52d2b421eafde5b3c36343d6d924a1e76a4c
Alumno <alumno@et26.local>
2026-03-30 17:34:45 -0300

    TrabajoIntroduccion30/03

---
+++ Introducción/JS/ejIntroduccionMEK.js
@@ -0,0 +1,6 @@
+function mostrarMeses(){
+    const meses = [
+        "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+    ];
+    console.log(meses)
+}

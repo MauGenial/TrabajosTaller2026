dd6668fa2a8ef7b0566a2a2548859129cd325c86
MauGenial <113713733+MauGenial@users.noreply.github.com>
2026-04-07 23:52:20 -0300

    08/04Trabajo

--- Introducción/JS/ej7MEKjs.js
+++ Introducción/JS/ej7MEKjs.js
@@ -1,5 +1,16 @@
-function ejercicio7(){
-    let frase = prompt("Introduzca una frase")
-    
-    
+function ejercicio7() {
+    let texto= prompt("Escriba un texto");
+
+    if (!isNaN(texto)) {
+        console.log("El texto contiene numeros.")
+    }
+    else if (texto === texto.toLowerCase()) {
+        console.log("El texto que escribio contiene solo minusculas");
+    }
+    else if(texto === texto.toUpperCase()){
+        console.log("El texto que escribio contiene solo mayusculas");
+    }
+    else {
+        console.log("El texto que escribio contiene tanto mayusculas como minusculas");
+    }
 }

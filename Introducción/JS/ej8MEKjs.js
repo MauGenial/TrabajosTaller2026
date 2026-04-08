248e2f9ae61436fbb53c1d99b2c53cb9453eafbb
MauGenial <113713733+MauGenial@users.noreply.github.com>
2026-04-08 00:03:36 -0300

    08/04Trabajo

--- Introducción/JS/ej8MEKjs.js
+++ Introducción/JS/ej8MEKjs.js
@@ -1,4 +1,12 @@
-function ejercicio8(){
-    let frase = prompt("Introduzca una frase")
-    
+function ejercicio8() {
+    let palin = prompt("Escriba un palindromo");
+    let str = palin.replaceAll(" ","").toLowerCase();
+    let inverStr = str.split("").reverse().join("");
+
+    if (str == arrStr) {  
+        console.log("En efecto, es un palindromo");
+    }
+    else{
+        console.log("Mentiroso... no e un palindromo eso");
+    }
 }
